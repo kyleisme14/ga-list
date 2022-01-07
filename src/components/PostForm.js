dd3cb25dc98313b6c115f555e705eeff52f12ad0
“kyleisme14” <kylekallman@gmail.com>
2022-01-07 14:27:16 -0700

    post form

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -14,22 +14,61 @@ class PostForm extends Component {
       this.setState({value: event.target.value});
     }
   
-    handleSubmit(event) {
-      alert('A name was submitted: ' + this.state.value);
-      event.preventDefault();
-    }
+    handleSubmit(){
+      let databody = {
+          "title": this.state.title,
+          "description": this.state.description,
+          "price": this.state.price,
+          "contact_info": this.state.contact_info,
+          "location": this.state.location,
+          "Image": this.state.image_url,
+      }
   
-    render() {
+      return fetch('http://localhost:5002/stored', {
+          method: 'POST',
+          body: JSON.stringify(databody),
+          headers: {
+              'Content-Type': 'application/json'
+          },
+      })
+      .then(res => res.json())
+      .then(data => console.log(data)); 
+  }
+  
+  
+  render(){
       return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
+          <div>
+              <form onSubmit={this.handleSubmit}>
+                  <div><label>
+                      Title
+                      <br/><input type="text" name="Title" value={this.title} onChange={this.handleTitle}/>
+                  </label> <br/></div>
+                  <div><label>
+                      Description
+                      <br/><input type="text" name="Description" value={this.description} onChange={this.handleDescription}/>
+                  </label> <br/></div>
+                  <div><label>
+                      Price
+                      <br/><input type="text" name="Price" value={this.price} onChange={this.handlePrice}/>
+                  </label> <br/></div>
+                  <div><label>
+                      Contact Info
+                      <br/><input type="text" name="Contact_info" value={this.contact_info} onChange={this.handleContact_info}/>
+                  </label> <br/></div>
+                  <div><label>
+                      Location
+                      <br/><input type="text" name="Location" value={this.location} onChange={this.handleLocation}/>
+                  </label> <br/></div>
+                  <div><label>
+                      Image
+                      <br/> <input type="text" name="Image_url" value={this.image_url} onChange={this.handleImage_url}/>
+                  </label> <br/></div>
+                  <input type="submit" value="Submit Post" />
+              </form> 
+          </div>
       );
-    }
+  }
   }
   
 
