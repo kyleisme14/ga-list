a671ff26ab691c6e0c8e9d60734e4f43d7a81458
Mikalai19 <bertman29@gmail.com>
2022-01-11 22:22:44 -0800

    better view of job data

--- src/components/Job.js
+++ src/components/Job.js
@@ -12,23 +12,22 @@ class Job extends Component {
   render() {
     return (
 
-
       <div>
 
-        <h3>Title:</h3>
-        {this.props.title}
-        <br />
-        <h3>Description:</h3>
-        {this.props.description}
-        <br />
-        <h3>Payment:</h3>
-        {this.props.payment}
-        <br />
-        <h3>Contact Info:</h3>
-        {this.props.contact_info}
-        <br />
-        <h3>Location:</h3>
-        {this.props.location}
+        <div class="tile is-parent is-shady">
+          <article class="tile is-child notification is-white">
+            <p class="title">Title:<br />{this.props.title}</p>
+            <p class="subtitle">Salary:{this.props.payment}</p>
+            <div class="content">
+              <p class="subtitle">Description:<br />{this.props.description}</p>
+              <p>Contact info: {this.props.contact_info}</p>
+            </div>
+            <div>
+              <p>Location: {this.props.location}</p>
+            </div>
+
+          </article>
+        </div>
 
       </div>
     )
